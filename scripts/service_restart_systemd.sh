0d54e3c921a522c54e032558d8b15a7ac30bcd6b
Malkiat Singh <43002915+malkiats@users.noreply.github.com>
2022-09-01 21:15:31 +0800

    Update service_restart_systemd.sh

--- scripts/service_restart_systemd.sh
+++ scripts/service_restart_systemd.sh
@@ -2,13 +2,13 @@
 
 #!/bin/bash
 serv=httpd
-sstat=dead
-systemctl status $serv | grep -i 'running\|dead' | awk '{print $3}' | sed 's/[()]//g' | while read output;
+stats=dead
+systemctl status $service | grep -i 'running\|dead' | awk '{print $3}' | sed 's/[()]//g' | while read output;
 do
 echo $output
-if [ "$output" == "$sstat" ]; then
+if [ "$output" == "$stats" ]; then
     systemctl start $serv
-    echo "$serv service is UP now.!" | mail -s "$serv service is DOWN and restarted now On $(hostname)" daygeek@gmail.com
+    echo "$serv service is UP now.!" | mail -s "$serv service is DOWN and restarted now On $(hostname)" malkiat.janjua@gmail.com
     else
     echo "$serv service is running"
     fi
